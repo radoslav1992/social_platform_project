a1d72e46e6efcbe71ee26f421fdbfd7dbbf495a0
Radoslav.Dodnikov <radoslav.dodnikov@gmail.com>
2020-11-03 17:52:45 +0200

    Initial commit of the project.

---
+++ src/test/java/com/radoslav/dodnikov/social_platform/SocialPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radoslav.dodnikov.social_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
