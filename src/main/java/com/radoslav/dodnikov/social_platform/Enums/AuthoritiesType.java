4c6e6b052622d5f61abca6cf35ebe97439b95012
Radoslav.Dodnikov <radoslav.dodnikov@gmail.com>
2020-11-04 14:23:31 +0200

    Created authorities and added persistance settings in application.properties.

---
+++ src/main/java/com/radoslav/dodnikov/social_platform/Enums/AuthoritiesType.java
@@ -0,0 +1,9 @@
+package com.radoslav.dodnikov.social_platform.Enums;
+
+public enum AuthoritiesType {
+
+    ROLE_REGULAR,
+    ROLE_NON_EDITING_ADMIN,
+    ROLE_ADMIN
+
+}
---
+++ src/main/java/com/radoslav/dodnikov/social_platform/users/authorities/Authority.java
@@ -0,0 +1,39 @@
+package com.radoslav.dodnikov.social_platform.users.authorities;
+
+import com.radoslav.dodnikov.social_platform.interfaces.AbstractEntity;
+import com.radoslav.dodnikov.social_platform.users.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "authorities")
+public class Authority extends AbstractEntity implements GrantedAuthority {
+
+    @Column(name = "authority")
+    private String authority;
+
+    @ManyToMany(mappedBy = "authorities")
+    private Set<User> users;
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
